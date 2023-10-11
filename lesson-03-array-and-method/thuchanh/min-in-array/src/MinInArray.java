68bd3ff089c41b20549effa457ea26e6025df624
Hgiang2 <vuhuonggiang9a@gmail.com>
2023-10-11 14:03:02 +0700

    done lesson 03 thuchanh

---
+++ lesson-03-array-and-method/thuchanh/min-in-array/src/MinInArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class MinInArray {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size: ");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.print("Property list: ");
+        for (int k : array) {
+            System.out.print(k + "\t");
+        }
+        int index = minValue(array);
+        System.out.println("The smallest element in the array is: " + array[index]);
+    }
+
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
